6a70ec42c93396324b03be2ec9bd5e974daaaf78
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-26 01:17:26 -0600

    Add build files to .gitignore & Add long-description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = "'Python program that visualizes data from a .csv file.'"
+LONG_DESCRIPTION = ""
 
 setup(
     name='data-visualizer',
     version='0.1.1',
-    description='Python program that visualizes data from a .csv file.',
+    description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     license='MIT',
     author='Asley R.',
     author_email='asleyrobleto@gmail.com',
