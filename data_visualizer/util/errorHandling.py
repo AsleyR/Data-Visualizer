107f222172ac6e01d66eb13999cce9229a3d521e
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-13 17:34:06 -0600

    clean code

--- data_visualizer/util/errorHandling.py
+++ data_visualizer/util/errorHandling.py
@@ -2,6 +2,7 @@ import pathlib
 from pydub import AudioSegment
 from pydub.playback import play
 
+
 def playErrorSound():
     ERROR_SOUND = pathlib.Path(__file__).parent.parent.resolve() / "media/audio/computer-error.wav"
     print(ERROR_SOUND)
